d122657fad983a2bda9fee2d1881b2d5f836eb63
kenta okamura <side.junktown@gmail.com>
2023-11-26 13:11:52 +0900

    Update artifact url

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libiconv",
-            url: "https://example.com/binaries/libiconv.zip",
-            checksum: "The checksum of the binary file"
+            url: "https://github.com/sidepelican/xciconv/releases/download/1.17.0/libiconv.xcframework.zip",
+            checksum: "72156afbdbcd8cb7c33f8503ddc1ed603cfd69b3c11790df8113eab102dd74c4"
         )
     ]
 )
